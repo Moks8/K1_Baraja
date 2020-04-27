86f6a95b81b5a0084450a934ee6babfbe2d0e02d
Moks8 <cristinagutierrezrub@gmail.com>
2020-04-27 21:55:15 +0200

    Baraja en función

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 cartas = ["A","2","3","4","5","6","7","S","C","R"]
 palos = ["o","c","b","e"]
 
-baraja = []
 
-for palo in palos:
-    for carta in cartas:
-        naipe = cartas + palos
-        baraja.append(naipe)
+
+def creaBaraja():
+    baraja = []
+    for palo in palos:
+        for carta in cartas:
+            naipe = carta + palo
+            baraja.append(naipe)
+
+    return baraja
+
+
+baraja1 = creaBaraja() # meter la lista en una variable para poder imprimir varias 
+baraja2 = creaBaraja()
+
+print(baraja1)
 '''
 for palo in range (len(palos)):
     for carta in range ( len(cartas)):
         naipe = cartas[carta] + palos [palo]
         baraja.append(naipe)
 
+si quiero crear la funcion con parameros podria ser crea_baraja(palos,cartas) y puedo crear las listas de la baraja francesa por ejemplo
 '''
 
-print(baraja)
